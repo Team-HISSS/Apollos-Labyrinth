c31c15cca3df71dabfe0ee84268149de436debf4
Shlok Agarwal <ashlok18@vt.edu>
2021-10-28 13:36:19 -0400

    Knight running and moving accross the screen --Version 2

--- knight_run_2021_10_27_00_23_39/sketch.js
+++ knight_run_2021_10_27_00_23_39/sketch.js
@@ -3,9 +3,36 @@ var runAnimation = [];
 function preload(){
   spriteSheet = loadImage('SpriteSheet.png');
 }
+class knightObj{
+  constructor(x, y, speed){
+    this.speed = speed;
+    this.index = 0;
+    this.x = x;
+    this.y = y
+    
+    
+  }
+  run(){
+  var curIndex = floor(this.index%runAnimation.length);
+    push();
+    scale(-1, 1);
+  // translate(-200, 200);
+  // rotate(PI/2);
+  
+    image(runAnimation[curIndex],this.x, this.y);
+    pop();
+  }
+  runAnimate(){
+    this.index += this.speed;
+    this.x -= this.speed*5;
+    if (this.x < -width){
+      this.x = 40;
+    }
+  }
+}
 var index = 0;
-var speed = 0.4;
-
+var speed = 0.3;
+var knight;
 function setup() {
   createCanvas(400, 400);
   background(220, 220, 220, 0);
@@ -21,21 +48,23 @@ function setup() {
   runAnimation.push(get(320, 206, 35, 43));
   runAnimation.push(get(355, 206, 40, 44));
   //imageMode(CENTER);
+  knight = new knightObj(-200, 200, 0.3);
   
 }
 
 function draw() {
   background(0, 255, 0);
   var curIndex = floor(index%runAnimation.length);
+  knight.run();
+  knight.runAnimate();
+//   push();
+//   scale(-1, 1);
+//   // translate(-200, 200);
+//   // rotate(PI/2);
   
-  push();
-  scale(-1, 1);
-  // translate(-200, 200);
-  // rotate(PI/2);
-  
-  image(runAnimation[curIndex],-200, 200);
-  index = index + speed;
-  pop();
+//   image(runAnimation[curIndex],-410, 200);
+//   index = index + speed;
+//   pop();
     // image(runAnimation[8],200, 200);
   
 }
