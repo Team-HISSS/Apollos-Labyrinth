f5bb15910fc99a6bd6bd265f8cbab9d077cbb59f
Sarang-R-119 <sarangvr@yahoo.com>
2021-10-29 19:00:41 -0400

    Changed the archer using a different spritesheet

--- archer/archer.js
+++ archer/archer.js
@@ -12,11 +12,13 @@ class Archer{
     this.action = action;
     this.chooseAction();
     this.animation = animation;
-    this.w = this.animation[0].width;
+    this.w = 100;
+    this.h = 100;
     this.x = x;
     this.y = y;
     this.index = 0;
     this.size = this.animation.length;
+    this.flip = 1;
   }
   
   // Chooses the action of the archer
@@ -43,16 +45,48 @@ class Archer{
   // Draws the frame
   draw(){
     let index = floor(this.index) % this.size;
-    image(this.animation[index], this.x, this.y);
+    // push();
+    // translate(this.x, this.y);
+    // rotate(PI/25);
+    if(this.index == 0){
+      this.flip *= -1;
+    }
+    
+    if(this.flip == 1){
+      push();
+      scale(-1, 1)
+      image(this.animation[index], -this.x, this.y, 200, 200);
+      pop();
+    }
+    else if(this.flip == -1){
+    image(this.animation[index], this.x, this.y, 200, 200);  
+    }
+    
+    // pop();
   }
   
   // Moves the archer relative to the canvas
   move(){
-    this.index += this.frameRate * 0.75;
-    this.x += this.speed * 2.00;
+    this.index += this.frameRate * 0.45;
+    this.x += this.speed * 3.00;
     if(this.x > width){
-      this.x = -this.w
+      this.x = -this.w;
     }
   }
   
+  move_down(){
+    this.index += this.frameRate * 0.75;
+    this.y += this.speed * 2.00;
+    if(this.y > height){
+      this.y = -this.h
+    }
+  }
+  
+   move_up(){
+    this.index += this.frameRate * 0.75;
+    this.y -= this.speed * 2.00;
+    if(this.y < -this.h){
+      this.y = height + this.h/2
+    }
+  }
 }
--- archer/sketch.js
+++ archer/sketch.js
@@ -1,30 +1,58 @@
 let spritesheet;
+let picture;
 
 function preload(){
   spritesheet = loadImage('spritesheet.png');
+  picture = loadImage('x.png');
 }
 
-let walk, run, shoot;
-let walkingAnimation = [];
-let shootingAnimation = [];
+let walk_up, run_right, run, shoot;
+// let walkingAnimation = [];
+// let shootingAnimation = [];
+// let walkingUpAnimation = [];
+let up = [];
+let right = [];
+
 function setup() {
   
   captureAllAnimation();
-  createCanvas(500, 500);
-  
-  walk = new Archer(walkingAnimation, 'w', 100, 100);
-  shoot = new Archer(shootingAnimation, 's', 100, 300);
-  run = new Archer(walkingAnimation, 'r', 100, 200);
+  createCanvas(4032, 976);
+  walk_up = new Archer(up, 'w', 100, 100);
+  run_right = new Archer(right, 'r', 100, 100);
+  // walk = new Archer(walkingAnimation, 'w', 100, 100);
+  // shoot = new Archer(shootingAnimation, 's', 100, 300);
+  // run = new Archer(walkingAnimation, 'r', 100, 200);
+  // walkUp = new Archer(walkingAnimation, 'w', 300, 100);
 }
-
+function grid(){
+  image(picture, 0, 0);
+  stroke(255);
+  for(let i = -1; i <= 2; i++){
+    line(150 + (i * 130), 0, 150 + (i * 130), height);
+  }
+  
+  // line(150, 0, 150, height);
+  // line(280, 0, 280, height);
+  // line(536, 0, 536, height);
+  
+  stroke(255, 0, 0);
+  for(let i = 0; i < 8; i++){
+    line(540 + (i * 127), 0, 536 + (i * 127), height);
+  }
+  
+  stroke(255);
+  for(let i = 0; i <= 8; i ++){
+    line(0, 122 * i, width, 122 * i);
+  }
+}
 
 function draw() {
   background(0);
-  walk.draw();
-  walk.move();
-  shoot.draw();
-  shoot.move();
-  run.draw();
-  run.move();
+  // grid();
+  // walk_up.draw();
+  // walk.move_up();
+  run_right.draw();
+  run_right.move();
+  
   
 }
